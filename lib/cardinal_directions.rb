b9d4ee40318e43ed0ea4d0a9883e00716baa1301
mulaiko <mulaikopwapwa@gmail.com>
2017-04-26 23:51:13 +0200

    formatting

--- lib/cardinal_directions.rb
+++ lib/cardinal_directions.rb
@@ -1,70 +1,70 @@
 module CardinalDirections
 
-	def initial_direction(direction)
-		return North.new if direction == "N"
-		return South.new if direction == "S"
-		return East.new if direction == "E"
-		return West.new if direction == "W"
-		raise MarsRoverError::InvalidInput
-	end
+  def initial_direction(direction)
+    return North.new if direction == "N"
+    return South.new if direction == "S"
+    return East.new if direction == "E"
+    return West.new if direction == "W"
+    raise MarsRoverError::InvalidInput
+  end
 
-	class North
+  class North
 
-	  def left
-			West.new
-		end
+    def left
+      West.new
+    end
 
-		def right
-			East.new
-		end
+    def right
+      East.new
+    end
 
-		def to_s
-			"N"
-		end
-	end
+    def to_s
+      "N"
+    end
+  end
 
-	class South
+  class South
 
-		def left
-			East.new
-		end
+    def left
+      East.new
+    end
 
-		def right
-			West.new
-		end
+    def right
+      West.new
+    end
 
-		def to_s
-			"S"
-		end
-	end
+    def to_s
+      "S"
+    end
+  end
 
-	class East
+  class East
 
-		def left
-		  North.new
-		end
+    def left
+      North.new
+    end
 
-		def right
-			South.new
-		end
+    def right
+      South.new
+    end
 
-		def to_s
-			"E"
-		end
-	end
+    def to_s
+      "E"
+    end
+  end
 
-	class West
+  class West
 
-		def left
-			South.new
-		end
+    def left
+      South.new
+    end
 
-		def right
-			North.new
-		end
+    def right
+      North.new
+    end
 
-		def to_s
-			"W"
-		end
-	end
+    def to_s
+      "W"
+    end
+  end
 end
